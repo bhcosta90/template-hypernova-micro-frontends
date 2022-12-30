f28153bd240d180b1174d7de87673fa1cbfa5c89
Bruno Costa <bruno.costa@dafiti.com.br>
2022-12-30 17:15:32 -0300

    retornado

--- hypernova-server-list-product/src/components/App.tsx
+++ hypernova-server-list-product/src/components/App.tsx
@@ -51,7 +51,7 @@ export const App = (props: AppProps) => {
           <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={rs.image} />
             <Card.Body>
-              <Card.Title>Product - {rs.title}</Card.Title>
+              <Card.Title>Product {rs.title}</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
@@ -63,7 +63,7 @@ export const App = (props: AppProps) => {
     </Row>
 
     <Button variant='primary' className='mt-3' onClick={() => addProduct()}>
-      Add product
+      Adicionar produto
     </Button>
 
   </Container>
