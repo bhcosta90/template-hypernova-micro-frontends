b6dcb4496e4edada560537ee6c1a7f4412422535
Bruno Costa <bruno.costa@dafiti.com.br>
2022-12-30 17:14:16 -0300

    finalizado

--- hypernova-server-list-product/src/components/App.tsx
+++ hypernova-server-list-product/src/components/App.tsx
@@ -16,7 +16,6 @@ type ProductProps = {
 }
 
 export const App = (props: AppProps) => {
-  // const [products, setProducts] = useState([]);
   const [products, setProducts] = useState<ProductProps[]>([]);
 
   useEffect(() => {
@@ -52,7 +51,7 @@ export const App = (props: AppProps) => {
           <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={rs.image} />
             <Card.Body>
-              <Card.Title>Product {rs.title}</Card.Title>
+              <Card.Title>Product - {rs.title}</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
